7d20bdfcc9b0991e5d095f7944c926e7f65a63fd
KovaKreative <eric.kovalevskyy@gmail.com>
2023-03-23 15:57:37 -0400

    Refactored my server code

--- express_server.js
+++ express_server.js
@@ -3,98 +3,102 @@ const cookieSession = require('cookie-session');
 const methodOverride = require('method-override');
 const bcrypt = require('bcryptjs');
 
-const { saveDatabase, loadDatabase } = require('./database');
-const { SmallURL, User } = require('./classes');
-const { getUser, getUserByEmail, fetchUserURLs, generateRandomString, checkIfHasProtocol } = require('./functions');
-
 const app = express();
 const PORT = 8080; // default port 8080
-
-
-const urlDatabase = loadDatabase('urlDatabase');
-const userDatabase = loadDatabase('userDatabase');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ name: 'session', keys: ['lazyEgg'], maxAge: 365 * 24 * 60 * 60 * 1000 }));
 app.use(methodOverride('_method'));
 
+const { SmallURL, User } = require('./classes');
+const { saveData, loadData } = require('./databaseFunctions');
+const { getUser, getUserByEmail, fetchUserURLs, generateRandomString, checkIfHasProtocol, castObjects, invalidatePost } = require('./helperFunctions');
 
-// READ HOME DIRECTORY - No specific request will default to list of urls if signed in, or login page if not
-app.get('/', (req, res) => {
-  const user = getUser(userDatabase, req.session.user_id);
-  if (user) {
-    res.redirect('/urls');
-    return;
-  }
-  res.redirect('/login');
-});
+const urlDatabase = castObjects(loadData('urlDatabase'), SmallURL.prototype);
+const userDatabase = castObjects(loadData('userDatabase'), User.prototype);
 
 //URL REQUESTS
 
 // BROWSE the list of URLs
 app.get('/urls', (req, res) => {
+
   const user = getUser(userDatabase, req.session.user_id);
+
   if (!user) {
     req.session = null;
-    return res.redirect("/login");
+    return res.redirect("/");
   }
+
   const userURLs = fetchUserURLs(urlDatabase, user.id);
+
   const templateVars = {
-    email: user ? user.email : undefined,
+    email: user.email,
     urls: userURLs
   };
+
   res.render("urls_index", templateVars);
 });
 
 // READ new URL form
 app.get("/urls/new", (req, res) => {
+
   const user = getUser(userDatabase, req.session.user_id);
+
   if (!user) {
-    res.redirect('/login');
-    return;
+    return res.redirect('/');
   }
+
   const templateVars = {
-    email: user ? user.email : undefined,
+    email: user.email
   };
+
   res.render("urls_new", templateVars);
 });
 
 // CREATE NEW small URL
 app.post('/urls', (req, res) => {
+
   const user = getUser(userDatabase, req.session.user_id);
   const longURL = req.body.longURL;
-  if (!checkIfHasProtocol(longURL)){
-    return res.status(400).render('urls_error', { email: user.email, message: "Please make sure to include the full URL (e.g. http://..., https://..., etc)" });
-  }
+
   if (!user) {
     return res.status(403).render('urls_error', { email: undefined, message: "You cannot generate a new tiny URL without an account. Please Log in or Register." });
   }
-  const urlID = generateRandomString(6);
-  while (Object.prototype.hasOwnProperty(urlID)) {
-    urlID = generateRandomString(6);
+
+  /*
+  I had to include the following check because without a protocol scheme,
+  the redirect function assumes that a given path is a part of the current domain
+  */
+  if (!checkIfHasProtocol(longURL)) {
+    return res.status(400).render('urls_error', { email: user.email, message: "Please make sure to include the full URL (e.g. http://..., https://..., etc)" });
   }
-  urlDatabase[urlID] = new SmallURL(req.body.longURL, user.id);
-  saveDatabase(urlDatabase);
+
+  const idDigits = 6;
+  const urlID = generateRandomString(idDigits);
+
+  // The following makes sure that by some freak of a coincidence, the generated ID isn't a match to an already existing one
+  while (Object.prototype.hasOwnProperty.call(urlDatabase, urlID)) {
+    urlID = generateRandomString(idDigits);
+  }
+
+  urlDatabase[urlID] = new SmallURL(longURL, user.id);
+
+  saveData(urlDatabase, "urlDatabase");
+
   res.redirect(`/urls/${urlID}`);
 });
 
-// READ individual entry for a tinyApp URL for Editing
+// READ individual page for a tinyApp URL for Editing
 app.get('/urls/:id', (req, res) => {
+
   const userID = req.session.user_id;
   const user = getUser(userDatabase, userID);
-  if (!user) {
-    return res.status(400).render('urls_error', { email: undefined, message: "You are not signed in." });
-  }
-
   const shortLinkItem = urlDatabase[req.params.id];
 
-  if (!shortLinkItem) {
-    return res.status(404).render('urls_error', { email: user.email, message: "Short link not found." });
-  }
-
-  if (shortLinkItem.userID !== userID) {
-    return res.status(403).render('urls_error', { email: user.email, message: "You do not have permission to edit this short link." });
+  const actionDenied = invalidatePost(user, shortLinkItem);
+  if (actionDenied) {
+    return res.status(actionDenied.code).render('urls_error', { email: undefined, message: actionDenied.message });
   }
 
   const templateVars = {
@@ -103,40 +107,40 @@ app.get('/urls/:id', (req, res) => {
     longURL: urlDatabase[req.params.id].longURL,
     visits: urlDatabase[req.params.id].visits
   };
-  console.log(templateVars);
+
   res.render("urls_show", templateVars);
+
 });
 
 // EDIT long URL on an existing entry
 app.put('/urls/:id', (req, res) => {
+
   const id = req.params.id;
   const longURL = req.body.longURL;
-  urlDatabase[id].longURL = longURL;
+  SmallURL.updateLongURL(urlDatabase[id], longURL);
+
   res.redirect(`/urls/${id}`);
+
 });
 
 // DELETE small URL
 app.delete('/urls/delete/:id', (req, res) => {
+
   const userID = req.session.user_id;
   const user = getUser(userDatabase, userID);
-  if (!user) {
-    return res.status(400).render('urls_error', { email: undefined, message: "You are not signed in." });
-  }
-
   const shortLinkItem = urlDatabase[req.params.id];
 
-  if (!shortLinkItem) {
-    return res.status(404).render('urls_error', { email: user.email, message: "Short link not found." });
-  }
-
-  if (shortLinkItem.userID !== userID) {
-    return res.status(403).render('urls_error', { email: user.email, message: "You do not have permission to delete this short link." });
+  const actionDenied = invalidatePost(user, shortLinkItem);
+  if (actionDenied) {
+    return res.status(actionDenied.code).render('urls_error', { email: undefined, message: actionDenied.message });
   }
 
   const id = req.params.id;
+
   if (urlDatabase[id]) {
     delete urlDatabase[id];
   }
+
   res.redirect(`/urls`);
 });
 
@@ -144,73 +148,90 @@ app.delete('/urls/delete/:id', (req, res) => {
 app.get("/u/:id", (req, res) => {
 
   const longURL = urlDatabase[req.params.id].longURL;
+
   if (!longURL) {
     return res.status(404).render('urls_error', { email: undefined, message: "Short URL not recognized." });
   }
-  console.log(req.session);
+
   let visitorID = generateRandomString(4);
-  if(req.session.visitor_id) {
+
+  if (req.session.visitor_id) {
     visitorID = req.session.visitor_id;
   } else {
     req.session.visitor_id = visitorID;
   }
-  urlDatabase[req.params.id].newVisit(visitorID, Date.now());
-  console.log(urlDatabase);
+
+  SmallURL.newVisit(urlDatabase[req.params.id], visitorID, Date.now());
+  saveData(urlDatabase, "urlDatabase");
+
   res.redirect(longURL);
 });
 
 //USER REQUESTS
 // READ registration form
 app.get('/register', (req, res) => {
+
   const user = getUser(userDatabase, req.session.user_id);
+
   if (user) {
-    res.redirect('/urls');
-    return;
+    return res.redirect('/urls');
   }
+
   res.render("urls_register", { email: undefined });
 });
 
 //CREATE a new user in the registry
 app.post('/register', (req, res) => {
-  id = generateRandomString(4);
+  
   const { email, password } = req.body;
+  
   if (!email || !password) {
-    res.status(400).render('urls_error', { email: undefined, message: "The email and password fields cannot be blank." });
-    return;
+    return res.status(400).render('urls_error', { email: undefined, message: "The email and password fields cannot be blank." });
   }
-  existingUser = getUserByEmail(userDatabase, email);
+  
+  const existingUser = getUserByEmail(userDatabase, email);
+  
   if (existingUser) {
-    res.status(400).render('urls_error', { email: undefined, message: "An account with that email address already exists." });
-    return;
+    return res.status(400).render('urls_error', { email: undefined, message: "An account with that email address already exists." });
   }
+  
+  const userDigits = 5;
+  const userID = generateRandomString(userDigits);
+  while (Object.prototype.hasOwnProperty.call(userDatabase, userID)) {
+    userID = generateRandomString(userDigits);
+  }
+
   const encryptedPassword = bcrypt.hashSync(password);
-  userDatabase[id] = new User(id, email, encryptedPassword);
-  saveDatabase(userDatabase);
-  req.session.user_id = id;
+  userDatabase[userID] = new User(userID, email, encryptedPassword);
+  saveData(userDatabase, "userDatabase");
+  
+  req.session.user_id = userID;
   res.redirect(`/urls`);
 });
 
-// READ login form
-app.get('/login', (req, res) => {
+// READ - DEFAULT PATH - If the user is signed in, it'll redirect to their urls, otherwise render the login form
+app.get('/', (req, res) => {
+
   const user = getUser(userDatabase, req.session.user_id);
 
   if (user) {
-    res.redirect('/urls');
-    return;
+    return res.redirect('/urls');
   }
+
   res.render("urls_login", { email: undefined });
 });
 
 // POST login in formation
 app.post('/login', (req, res) => {
+
   const user = getUserByEmail(userDatabase, req.body.email);
   const pass = req.body.password;
 
   if (user && bcrypt.compareSync(pass, user.password)) {
     req.session.user_id = user.id;
-    res.redirect('/urls');
-    return;
+    return res.redirect('/urls');
   }
+
   res.status(403).render('urls_error', { email: undefined, message: "Your email and/or password does not match our records." });
 });
 
@@ -219,7 +240,6 @@ app.post('/logout', (req, res) => {
   req.session = null;
   res.redirect(`/login`);
 });
-
 
 // BROWSE **For testing purposes, not for deployment
 app.get('/urls.json', (req, res) => {
