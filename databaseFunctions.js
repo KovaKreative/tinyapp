4be2546ab908153959a8f255234fb584645fc25b
KovaKreative <eric.kovalevskyy@gmail.com>
2023-03-23 19:22:53 -0400

    Added descriptions to the database functions

--- databaseFunctions.js
+++ databaseFunctions.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
 
+/**
+ * Takes in a list of objects and uses JSON Stringify to turn it into a string
+ * before saving it into the filename specified in the second parameter in a folder
+ * called 'database'. The folder must already exist for this function to
+ * work.
+ * @param {Object} data 
+ * @param {String} path 
+ */
 const saveData = function(data, path) {
   const fileData = JSON.stringify(data);
   fs.writeFile(`./database/${path}`, fileData, (error) => {
@@ -10,10 +18,14 @@ const saveData = function(data, path) {
   });
 };
 
-/* 
-Choosing to use readFileSync because as it is now, I don't want to start my server until I fetch my databases, so doing it async is pointless
-In the future, if the number of users increases, it might be worth keeping a separate file for every user and just load them as needed when a given user logs in
-*/
+/**
+ * Loads the specified filename from the database folder, parses its contents and returns
+ * it as an object. The database folder and the specified filename must already exist
+ * for this function to work. 
+ * @param {String} path
+ * The file by that name must already exist for the operation to be successful.
+ * readFileSync was chosen because the server should not start until the databases are loaded.
+  */
 const loadData = function(path) {
   const database = fs.readFileSync(`./database/${path}`, 'utf-8', (error, data) => {
     if (error) {
